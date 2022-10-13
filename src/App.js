48782aaf9fc5051a9ef58d99a5bfa5f7b83a4180
yamunasrikonda <yamuna.jakkam91@gmail.com>
2022-10-13 17:06:07 +0100

    react

--- src/App.js
+++ src/App.js
@@ -3,8 +3,9 @@ import './App.css';
 import Person from './Person';
 import Products from './components/props/Products';
 import Parent from './components/props/Parent';
-import TrueFalse from './components/States/TrueFalse';
-import Counter from './components/States/Counter';
+//import TrueFalse from './components/States/TrueFalse';
+//import Counter from './components/States/Counter';
+import Login from './components/States/Login';
 
 function App() {
 
@@ -12,8 +13,9 @@ function App() {
   return (
     <div className="App">
 
-    <TrueFalse/>
-    <Counter/>
+    <Login/>
+    
+
     </div>
   );
 }
