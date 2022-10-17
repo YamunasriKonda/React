dcec72c71daae01186f2e3c3b8b8251ec7a9d0ee
yamunasrikonda <yamuna.jakkam9@gmail.com>
2022-10-17 15:11:03 +0100

    datarequests

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import FilmReq from './FilmReq';
+import React from 'react';
+
+function App() {
+
+  //const Heading = () => <h1>Login!</h1>
+
+  return (
+    <div className="App">
+      <FilmReq/>
+    </div>
+  );
+}
+
+export default App;
