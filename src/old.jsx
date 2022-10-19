41990a08cbb1410b9cd513727ccea511464188e7
yamunasrikonda <yamuna.jakkam91@gmail.com>
2022-10-19 14:54:53 +0100

    reactrouting

--- src/old.jsx
+++ src/old.jsx
@@ -1,35 +1,35 @@
 import React from 'react';
 import { Routes, Route, BrowserRouter as Router, Link } from 'react-router-dom';
 import './App.css';
-import Home from './Home'
+import Home from './Home';
 import Users from './Users';
 import Contact from './contact';
 
 function App() {
   return (
     <Router>
-
       <div>
         <ul>
           <li>
             <Link to="/">Home</Link>
-            </li>
-            <li>
+          </li>
+          <li>
             <Link to="/users">Users</Link>
-            </li>
-            <li>
+          </li>
+          <li>
             <Link to="/contact">Contact</Link>
           </li>
         </ul>
-
+        <li>
+          <Link to="/notfound">NotFound</Link>
+        </li>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />}/>
-          <Route path="/users" element={<Users />} />
-          
-
+          <Route exact path="/"> <Home /> </Route>
+          <Route path="/users/:id"><Users /> </Route>
+          <Route path="/users"> <Users /> </Route>
+          <Route path="/contact" > <Contact /> </Route>
+          <Route path="/notfound" > <Contact /> </Route>
         </Routes>
-
 
 
       </div>
