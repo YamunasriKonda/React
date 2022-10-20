8182e86a0599ebbfa50a81294d64678efe4d2e3d
yamunasrikonda <yamuna.jakkam91@gmail.com>
2022-10-20 14:27:32 +0100

    pp

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,82 @@
-import React from 'react';
-import { Routes, Route, BrowserRouter as Router, Link } from 'react-router-dom';
-import './App.css';
-import Home from './Home'
-import Users from './Users';
-import Contact from './contact';
-import notfound from './notfound';
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Link, Route, Routes,} from 'react-router-dom';
+import { Button, Col, Container, Nav, Row } from 'react-bootstrap';
+import Poke from './Components/Data-Requests/Poke';
+import Counter from './Components/States/Counter';
+import OMDB from './Components/Data-Requests/OMDB';
+import SearchableList from './Components/Lifting-State/SearchableList';
+import Home from './Components/Routing/Home';
+import Navigation from './Components/Routing/Navigation';
+import ParamsExample from './Components/Routing/ParamsExample';
+import People from './Components/Static-Data/People';
+import FilmDetails from './Components/Routing/FilmDetails';
 
 function App() {
   return (
-    <Router>
+    <div className="App">
+      <Router>
+        <Nav>
+          <Container>
+            <Row>
+              <Col>
+                <Nav.Link href="/home">
+                  Home
+                </Nav.Link>
+              </Col>
+              <Col>
+                <Link to="/liftingState">
+                  <Button variant="primary" type="button">Lifting State</Button>
+                </Link>
+              </Col>
+              <Col>
+                <Link className="nav-link" to="/state">
+                  State
+                </Link>
+              </Col>
+              <Col>
+                <Link to="/staticData">
+                  Static Data
+                </Link>
+              </Col>
+              <Col>
+                <Link to="/dataRequests/example">
+                  Data Req - Example
+                </Link>
+              </Col>
+              <Col>
+                <Link to="/filmSearch">
+                  Film Search
+                </Link>
+              </Col>
+              <Col lg={3}>
+                <Navigation />
+              </Col>
+            </Row>
+          </Container>
 
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/users">Users</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/notfound">notfound</Link>
-          </li>
-        </ul>
-
+        </Nav>
+        <br />
+        <header>
+          <h1>I AM A HEADER</h1>
+        </header>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/users" element={<Users />} />
-          <Route path="/notfound" element={<notFound />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/state" element={<Counter />} />
+          <Route path="/staticData" element={<People />} />
+          <Route path="/liftingState" element={<SearchableList />} />
+          <Route path="/dataRequests/example" element={<Poke />} />
+          <Route path="/filmSearch" element={<OMDB />} />
+          <Route path="/filmDetails/:id" element={<FilmDetails />} />
+          <Route path="/params/:colour" element={<ParamsExample />} />
         </Routes>
-      </div>
-    </Router>
+        <footer>
+          I AM A FOOTER
+        </footer>
+      </Router>
+      {/* <Converter/> */}
+      {/* <TrueFalse/> */}
+    </div>
   );
 }
 
